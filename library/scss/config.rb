361dc7ffd77900e2cec5643e84be3a9c369eaef9
James <james@greenhousegroup.co.uk>
2014-03-20 15:25:03 +0000

    Moving style.css to theme root on compile and save

--- library/scss/config.rb
+++ library/scss/config.rb
@@ -32,6 +32,6 @@ require 'fileutils'
 on_stylesheet_saved do |file|
   if File.exists?(file) && File.basename(file) == "style.css"
     puts "Moving: #{file}"
-    FileUtils.mv(file, File.dirname(file) + "/../" + File.basename(file))
+    FileUtils.mv(file, File.dirname(file) + "/../../" + File.basename(file))
   end
 end
